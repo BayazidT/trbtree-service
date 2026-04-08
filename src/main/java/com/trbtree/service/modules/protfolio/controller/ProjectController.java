448f497c24a112553c08c93948af812e48c48b3c
bayazidt <btnirob13@gmail.com>
2026-04-08 23:53:51 +0200

    Project creation continued

--- src/main/java/com/trbtree/service/modules/protfolio/controller/ProjectController.java
+++ src/main/java/com/trbtree/service/modules/protfolio/controller/ProjectController.java
@@ -1,6 +1,8 @@
 package com.trbtree.service.modules.protfolio.controller;
 
+import com.trbtree.service.modules.protfolio.dto.ProjectResponse;
 import com.trbtree.service.modules.protfolio.dto.SkillResponse;
+import com.trbtree.service.modules.protfolio.service.ProjectService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,10 +18,12 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class ProjectController {
 
+    private final ProjectService projectService;
+
 
     @GetMapping("/{userId}")
-    public ResponseEntity<List<SkillResponse>> getProject(@PathVariable UUID userId) {
-        List<SkillResponse> responseList = skillService.getSkill(userId);
+    public ResponseEntity<List<ProjectResponse>> getProject(@PathVariable UUID userId) {
+        List<ProjectResponse> responseList = projectService.getProject(userId);
         return ResponseEntity.ok(responseList);
     }
 }
--- src/main/java/com/trbtree/service/modules/protfolio/service/impl/ProjectServiceImpl.java
+++ src/main/java/com/trbtree/service/modules/protfolio/service/impl/ProjectServiceImpl.java
@@ -1,6 +1,16 @@
 package com.trbtree.service.modules.protfolio.service.impl;
 
+import com.trbtree.service.modules.protfolio.dto.ProjectResponse;
 import com.trbtree.service.modules.protfolio.service.ProjectService;
+import lombok.AllArgsConstructor;
 
+import java.util.List;
+import java.util.UUID;
+
+@AllArgsConstructor
 public class ProjectServiceImpl implements ProjectService {
+    @Override
+    public List<ProjectResponse> getProject(UUID userId) {
+        return List.of();
+    }
 }
