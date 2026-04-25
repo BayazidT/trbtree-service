b9c215b0c5fce2d589ba020a7aa44cc6a8d07feb
bayazidt <btnirob13@gmail.com>
2026-04-25 02:04:45 +0200

    message retrival added

--- src/main/java/com/trbtree/service/modules/branch/service/ConversationService.java
+++ src/main/java/com/trbtree/service/modules/branch/service/ConversationService.java
@@ -1,7 +1,13 @@
 package com.trbtree.service.modules.branch.service;
 
+import com.trbtree.service.modules.branch.dto.ConversationResponse;
 import com.trbtree.service.modules.branch.dto.CreateConversationRequest;
+
+import java.util.List;
+import java.util.UUID;
 
 public interface ConversationService {
     void startConversation(CreateConversationRequest request);
+
+    List<ConversationResponse> getConversations(UUID userId);
 }
