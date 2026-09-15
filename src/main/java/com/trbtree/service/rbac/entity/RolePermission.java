ef7e1f4a9bd8dd1f886696af2534439c04e41155
bayazidt <btnirob13@gmail.com>
2026-09-16 01:15:42 +0200

    few typos corrected

--- src/main/java/com/trbtree/service/rbac/entity/RolePermission.java
+++ src/main/java/com/trbtree/service/rbac/entity/RolePermission.java
@@ -9,7 +9,7 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
-@Table(name = "role_permissions", schema = "infotree")
+@Table(name = "role_permissions", schema = "trbtree")
 @IdClass(RolePermissionId.class)
 @Getter @Setter @Builder
 @NoArgsConstructor @AllArgsConstructor
@@ -33,7 +33,4 @@ public class RolePermission {
     @JoinColumn(name = "permission_id")
     private Permission permission;
 
-    @CreationTimestamp
-    @Column(name = "granted_at")
-    private LocalDateTime grantedAt;
 }
